bd92e4d0bedc68f1f1ba6503b480cf25a6a88ab9
ankutalev <a.kutalev@g.nsu.ru>
2019-02-12 10:07:43 +0700

    Scheduler done

---
+++ Scheduler/Scheduler/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Scheduler {
+    class Scheduler {
+        private DateTime _inputData;
+        private String _inputMessage = "Enter date, please!";
+
+        public void ReadDate() {
+            bool isCorrectDate = false;
+            while (!isCorrectDate) {
+                Console.WriteLine(_inputMessage);
+                var userInput = Console.ReadLine();
+                isCorrectDate = DateTime.TryParse(userInput, out _inputData);
+            }
+        }
+
+        public void DrawSchedule() {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Input data {0}", _inputData);
+            Console.ResetColor();
+            var knowWeekDay = new DateTime(2019, 02, 11); // surely first day of the week
+            for (int i = 0; i < 7; i++) {
+                Console.Write("{0:ddd}  ", knowWeekDay);
+                knowWeekDay = knowWeekDay.AddDays(1);
+            }
+            Console.WriteLine();            
+            var monthBeginning = new DateTime(_inputData.Year, _inputData.Month, 1);
+            var workDays = 0;
+            for (int i = 0; i < ((int) monthBeginning.DayOfWeek + 6) % 7; i++) {
+                Console.Write("    ");
+            }
+            while (monthBeginning.Month == _inputData.Month) {
+                if (monthBeginning.DayOfWeek == DayOfWeek.Sunday || monthBeginning.DayOfWeek == DayOfWeek.Saturday) {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else {
+                    Console.ResetColor();
+                    workDays++;
+                }
+                if (monthBeginning.Day == _inputData.Day)
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("{0,2}  ", monthBeginning.Day);
+                if (monthBeginning.DayOfWeek == DayOfWeek.Sunday) {
+                    Console.WriteLine();
+                }
+                monthBeginning = monthBeginning.AddDays(1);
+            }
+            Console.WriteLine();
+            Console.Write(" Work days without holidays in this month: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(workDays);
+        }
+        static void Main(string[] args) {
+            Scheduler scheduler = new Scheduler();
+            scheduler.ReadDate();
+            scheduler.DrawSchedule();
+        }
+    }
+}
