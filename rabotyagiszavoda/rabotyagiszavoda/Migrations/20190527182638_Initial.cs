b853570e75dfb6c7ecf0442a3aecee9f56efb591
ankutalev <a.kutalev@g.nsu.ru>
2019-05-28 01:28:10 +0700

    GIT ADD TOCHKA

---
+++ rabotyagiszavoda/rabotyagiszavoda/Migrations/20190527182638_Initial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace rabotyagiszavoda.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            
+        }
+    }
+}
