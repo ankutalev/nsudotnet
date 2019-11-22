b1d98d860905e861cf1b01071ef67765eaee9c12
ankutalev <a.kutalev@g.nsu.ru>
2019-11-23 01:22:41 +0700

    Nested Panel add

---
+++ Booknote/BooknoteView/RecordsList.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Controls;
+
+namespace BooknoteView
+{
+    public partial class RecordsList : UserControl
+    {
+        public RecordsList()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+        }
+    }
+}
